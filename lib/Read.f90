67709b5a8c8a94e22ea02af133d4172b24501663
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2014-10-31 16:32:45 +0000

    Add reading pi-pulse duration Tau

--- lib/Read.f90
+++ lib/Read.f90
@@ -57,6 +57,7 @@ contains
     read(Dynamics, fmt3) Tmax
     read(Dynamics, fmt2) nb_pts_t
     read(Dynamics, fmt4) Dynadeco
+    if (Dynadeco == "Hahn" .or. Dynadeco == "DD") read(Dynamics, fmt3) Tau
     if (Qubittype == "nuclear") read(Dynamics, fmt3) Jnuc
     close(Dynamics)
 
