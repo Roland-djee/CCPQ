c7e0365567e31ce2eb3bebcdf02085fadf242ad6
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2014-11-07 11:06:18 +0000

    Grouping variables

--- lib/Types.f90
+++ lib/Types.f90
@@ -8,7 +8,9 @@ module types
   integer :: origin
 
   integer :: nb_pts_t
-  double precision :: Tmax, Tau, polar_up, polar_down, Jnuc
+  double precision :: Tmax, Tau
+  double precision :: polar_up, polar_down
+  double precision :: Jnuc
 
   logical :: crys_sites
   logical :: crys_imp
