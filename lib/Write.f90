ba2699eee4c477af5f9d7e0155ce349823b145e0
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2014-11-18 11:50:10 +0000

    More precise writting formats

--- lib/Write.f90
+++ lib/Write.f90
@@ -2,11 +2,20 @@ module write
   use types
   implicit none
   ! writing formats
-  character (len=*), parameter :: fmt0 = "(a)"
+  character (len=*), parameter :: fmt01 = "(a1, t10, a, t25, a, t40, a)"
+  character (len=*), parameter :: fmt02 = "(a1, t5, a, t10, a, t15, a,&
+                                            t20, a, t25, a, t30, a, t40, a)"
+  character (len=*), parameter :: fmt03 = "(a1, t10, a, t25, a, t41, a,&
+                                            t61, a, t81, a)"
+  character (len=*), parameter :: fmt04 = "(a1, t5, a, t10, a, t15, a,&
+                                            t20, a, t25, a, t30, a, &
+                                            t41, a, t61, a, t81, a)"
+  character (len=*), parameter :: fmt05 = "(a1, t11, a, t31, a, t51, a, t65, a)"
   character (len=*), parameter :: fmt1 = "(i5, i5, i5)"
   character (len=*), parameter :: fmt2 = "(i5, i5, i5, i5, i5, i5, es20.10e3)"
   character (len=*), parameter :: fmt3 = "(i5, i5, i5, i5, i5, i5, es20.10e3,&
        es20.10e3, es20.10e3)"
+  character (len=*), parameter :: fmt4 = "(es20.10e3, es20.10e3, es20.10e3)"
   
 contains
 
@@ -20,23 +29,24 @@ contains
     open(16,file='couplings.dat')
     
     ! Headers
-    write(14,fmt0)"# Coordinates spin 1 [n1,n2,n3] Coordinates spin 2 &
-         C12 [rad/s]"
-    write(15,fmt0)"# Coordinates spin 1 [n1,n2,n3] Coordinates spin 2 &
-         [n1,n2,n3] J1 J2 DJ [rad/s]"
-    write(16,fmt0)"# J1 J2 C12 [rad/s]"
+    write(14, fmt01)"#","spin 1","spin 2","C12"
+    write(14, fmt02)"#","nx,","ny,","nz","nx,","ny,","nz","[rad/s]"
+    write(15, fmt03)"#","spin 1","spin 2","J1","J2","DJ"
+    write(15, fmt04)"#","nx,","ny,","nz","nx,","ny,","nz",&
+                    "[rad/s]","[rad/s]","[rad/s]"
+    write(16, fmt05)"#","J1","J2","C12","[rad/s]"
 
     m = 0
     do k=1,nb_imp - 1
        do l=k + 1,nb_imp
-          m = m + 1
-          write(14,fmt2)lattice_imp(k)%x, lattice_imp(k)%y, lattice_imp(k)%z,&
+          m = m + 1 
+          write(14, fmt2)lattice_imp(k)%x, lattice_imp(k)%y, lattice_imp(k)%z,&
                         lattice_imp(l)%x, lattice_imp(l)%y, lattice_imp(l)%z,&
                         C12(m)
-          write(15,fmt3)lattice_imp(k)%x, lattice_imp(k)%y, lattice_imp(k)%z,&
+          write(15, fmt3)lattice_imp(k)%x, lattice_imp(k)%y, lattice_imp(k)%z,&
                         lattice_imp(l)%x, lattice_imp(l)%y, lattice_imp(l)%z,&
                         J(k), J(l), DJ(m)       
-          write(16, "(es20.10e3, es20.10e3, es20.10e3)")J(k), J(l), C12(m)
+          write(16, fmt4)J(k), J(l), C12(m)
        end do
     end do
     close(14)
