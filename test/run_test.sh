3ead3ca2fb020980070c551454c5b01ba8766602
Roland Guichard <rguichard@jules.theory.phys.ucl.ac.uk>
2014-11-19 15:55:55 +0000

    Add run testing script

--- test/run_test.sh
+++ test/run_test.sh
@@ -13,15 +13,25 @@ rm *.dat
 ./Decoherence.exe
 
 # Copy output files as inputs for Tania's subroutine
-
 cd ../test/Tania_subroutine/
-rm *.dat*
+rm *.dat
 
 lines=$(wc -l < ../../bin/couplings_nuclear_*.dat)
 echo $((lines - 1)) > coupling.dat
 sed -n '2,$p' ../../bin/couplings_nuclear_*.dat >> coupling.dat
 
+# Run test
 ./ROLAND.DD.out < input_test.inp
+
+# copy back and remove input files
+cp ../../input/System_tmp.inp ../../input/System.inp 
+cp ../../input/Dynamics_tmp.inp ../../input/Dynamics.inp 
+cp ../../input/Output_tmp.inp ../../input/Output.inp 
+
+rm ../../input/System_tmp.inp
+rm ../../input/Dynamics_tmp.inp
+rm ../../input/Output_tmp.inp
+
 
 
 
